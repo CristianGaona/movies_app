24292a4f170761858fc06d713b4fec57e4bd41f3
CristianGaona <crgaonas24@gmail.com>
2022-08-02 21:32:57 -0500

    added description about movies and actors

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
@@ -8,10 +9,18 @@ class DetailsScreen extends StatelessWidget {
     final String movie =
         ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [_CustomAppBar()],
-      ),
-    );
+        body: CustomScrollView(
+      slivers: [
+        const _CustomAppBar(),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          const _PosterAndTitle(),
+          const _Overview(),
+          const _Overview(),
+          const CastingCards()
+        ]))
+      ],
+    ));
   }
 }
 
@@ -32,6 +41,7 @@ class _CustomAppBar extends StatelessWidget {
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           color: Colors.black12,
+          padding: const EdgeInsets.only(bottom: 10),
           child: const Text('movie.title', style: TextStyle(fontSize: 16)),
         ),
         background: const FadeInImage(
@@ -41,5 +51,76 @@ class _CustomAppBar extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+                image: NetworkImage('http://via.placeholder.com/200x300'),
+                height: 150,
+                placeholder: AssetImage('assets/no-image.jpg' ''))),
+        const SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'movie.title',
+              style: textTheme.headline5,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            Text(
+              'movie.originalTitle',
+              style: textTheme.subtitle1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.star_outline,
+                  size: 15,
+                  color: Colors.grey,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'movie.voteAverage',
+                  style: textTheme.caption,
+                )
+              ],
+            )
+          ],
+        )
+      ]),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: Text(
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+          textAlign: TextAlign.justify,
+          style: Theme.of(context).textTheme.subtitle1,
+        ));
   }
 }
